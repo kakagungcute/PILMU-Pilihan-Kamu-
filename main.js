dfad84631f58821db5b63f85af162a6578182b75
syfactr <auxiliadoradezena0@gmail.com>
2024-07-09 23:22:11 +0700

    kesalahan push menggunakan akun kakagungcute,yang seharusnya pake akun syfctr

--- main.js
+++ main.js
@@ -26,11 +26,3 @@ document.querySelectorAll('.btnDetail').forEach(item => {
         document.querySelector('.btnBeli').href = pesan;
     });
 });
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
